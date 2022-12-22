9de861c317d07a249824bbceaf421ea703588264
shirai0323 <aki0323hiro@icloud.com>
2022-12-23 01:52:05 +0900

    [Add]ユーザー登録時にセレクト形式で性別選択の実装

--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,8 @@ module Mokumoku
     end
 
     config.i18n.default_locale = :ja
-    # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.available_locales = [:ja, :en]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
   end
